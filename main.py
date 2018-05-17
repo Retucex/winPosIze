4969e18e1f5961fb9623abc944f53df6c5c8b44c
Samuel Leclerc <sleclerc@Gestisoft.com>
2018-05-17 16:48:52 -0400

    Started class Win32Placement

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import win32process as process
 import win32gui as gui
-from win32helper import Win32Window
+from win32helper import Win32Window, Win32WindowPlacement
 
 def cb(hwnd, listwin):
     win = Win32Window(hwnd)
     if win.isVisible and len(win.title) > 0:
         listwin.append(Win32Window(hwnd))
 
-
 windows = []
 gui.EnumWindows(cb, windows)
 print(gui.GetCursorInfo())
+
 for w in windows:
     print(w)
--- win32helper.py
+++ win32helper.py
@@ -1,16 +1,20 @@
 import win32gui as gui
 
+class Win32WindowPlacement:
+    def __init__(self, placement=None):
+        if placement != None:
+            (self.flags, self.showCmd,
+            (self.minPosX, self.minPosY),
+            (self.maxPosX, self.maxPosY),
+            (self.posXTopLeft, self.posYTopLeft, self.posXBottomRight, self.posYBottomRight)) = placement
+
+
 class Win32Window:
     def __init__(self, hwnd):
         self.hwnd = hwnd
         self.title = gui.GetWindowText(hwnd)
         self.isVisible = gui.IsWindowVisible(hwnd)
-        self.placement = gui.GetWindowPlacement(hwnd)
-
-        (self.flags, self.showCmd,
-        (self.minPosX, self.minPosY),
-        (self.maxPosX, self.maxPosY),
-        (self.posXTopLeft, self.posYTopLeft, self.posXBottomRight, self.posYBottomRight)) = self.placement
+        self.placement = Win32WindowPlacement(gui.GetWindowPlacement(hwnd))
 
     def __str__(self):
         return f"[{self.hwnd}] - {self.title}" 
